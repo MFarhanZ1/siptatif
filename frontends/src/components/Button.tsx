85e71367b1dc6bd6acc12648bcaad5b1b19f5de5
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-09 10:17:00 +0700

    style: fixing custom button component code and adjusting its customizable attributes

--- frontends/src/components/Button.tsx
+++ frontends/src/components/Button.tsx
@@ -1,13 +1,21 @@
-
-interface Props {
-  className: string;
-  text : string;
-  disabled?: boolean;
-  onClick?: () => void;
+interface ButtonProps {
+	className: string;
+	text: string;
+	disabled?: boolean;
+	onClick?: () => void;
 }
 
-function Button ({className, text, onClick, disabled}: Props) {
-  return <button className={`px-4 py-1 h-12 text-center ${className}`} disabled={disabled} type='submit' onClick={onClick}>{text}</button>;
-}
+const Button = ({ className, text, onClick, disabled }: ButtonProps) => {
+	return (
+		<button
+			className={`px-4 py-1 h-12 text-center ${className}`}
+			disabled={disabled}
+			type="submit"
+			onClick={onClick}
+		>
+			{text}
+		</button>
+	);
+};
 
 export default Button;
