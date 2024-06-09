2a5d5106337e55a6ef05d1b92383ffcb085bbe8f
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-09 10:40:09 +0700

    fix: repaired a dischanging cursor from pointer to not-allowed which caused by tailwind just in time features

--- frontends/tailwind.config.js
+++ frontends/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
+  safelist: [
+    'cursor-pointer',
+    'cursor-not-allowed',
+  ],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"
