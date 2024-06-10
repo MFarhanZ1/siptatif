9ba4acd91f4660728a2fc0af1f4ffc3a21de7c6f
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-10 17:22:41 +0700

    feat: adding login and register logic endpoints also argon2 as hash manager

--- backends/index.js
+++ backends/index.js
@@ -6,7 +6,8 @@ const cors = require("cors");
 const validasi_email = require('./middlewares/validasi_email');
 
 // importing services
-const { verifikasi_token, kirim_link_verifikasi} = require('./services/auth_register');
+const { verifikasi_token, kirim_link_verifikasi, register_akun_mahasiswa} = require('./services/register');
+const { verifikasi_login } = require("./services/login");
 
 require('./utils/cleanup_expires_token');
 require('dotenv').config();
@@ -21,6 +22,8 @@ app.use(express.json());
 // endpoint routes
 app.post("/kirim-link-verifikasi", validasi_email, kirim_link_verifikasi);
 app.post("/verifikasi-token", verifikasi_token);
+app.post("/register", register_akun_mahasiswa);
+app.post("/login", verifikasi_login);
 
 app.listen(port, () => {
 	console.log(`SIPTATIF API Started on port ${port}`);
