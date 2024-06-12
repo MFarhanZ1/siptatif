9b0a0e9fe598fcae9bfa6550c23f5f4387b95121
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-12 19:55:09 +0700

    feat: adding new column is_verified in unverified_email table to give backend info is verif email was clicked or not

--- databases/procedures/lupa_password_procedures.sql
+++ databases/procedures/lupa_password_procedures.sql
@@ -1,7 +1,9 @@
+DROP TABLE IF EXISTS unverified_email_lupa_password CASCADE;
 CREATE TABLE IF NOT EXISTS unverified_email_lupa_password (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     verification_token VARCHAR(255) NOT NULL,
+    is_verified BOOLEAN NOT NULL DEFAULT FALSE,
     expires_at TIMESTAMP NOT NULL
 );
 
@@ -12,6 +14,10 @@ CREATE OR REPLACE PROCEDURE reset_password(
 LANGUAGE plpgsql
 AS $$
 BEGIN
+    -- memperbarui password akun mahasiswa dengan password yang baru, dan mengupdate timestamp
     UPDATE akun SET password = getPassword, updated_at = NOW() WHERE email = getEmail;
+
+    -- menghapus data unverified email
+    DELETE FROM unverified_email_lupa_password WHERE email = getEmail;
 END;
 $$;
--- databases/procedures/register_procedures.sql
+++ databases/procedures/register_procedures.sql
@@ -1,12 +1,14 @@
+DROP TABLE IF EXISTS unverified_email_register CASCADE;
 CREATE TABLE IF NOT EXISTS unverified_email_register(
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     verification_token VARCHAR(255) NOT NULL,
+	is_verified BOOLEAN NOT NULL DEFAULT FALSE,
     expires_at TIMESTAMP NOT NULL
 );
 
 CREATE OR REPLACE PROCEDURE register_akun_mahasiswa(
-	email				VARCHAR,
+	getEmail			VARCHAR,
 	password		 	VARCHAR,
 	nim					VARCHAR,
 	nama				VARCHAR,
@@ -17,9 +19,12 @@ LANGUAGE plpgsql
 AS $$
 BEGIN
 	-- buatkan akun mahasiswa + role bakal auto jadi (3) alias mahasiswa
-	INSERT INTO akun(email, password, id_role) VALUES (email, password, 3);
+	INSERT INTO akun(email, password, id_role) VALUES (getEmail, password, 3);
 
 	-- tambahkan data mahasiswa
-	INSERT INTO mahasiswa(nim, nama, tanggal_lahir, no_hp, email) VALUES (nim, nama, tanggal_lahir, no_hp, email);
+	INSERT INTO mahasiswa(nim, nama, tanggal_lahir, no_hp, email) VALUES (nim, nama, tanggal_lahir, no_hp, getEmail);
+
+	-- hapus data unverified email
+	DELETE FROM unverified_email_register WHERE email = getEmail;
 END;
 $$;
