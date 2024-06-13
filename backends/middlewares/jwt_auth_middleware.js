8edd1787afb9e4ad8e44ed41ec1777df65b5531f
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-13 15:05:20 +0700

    feat: adding middlewares for verifying access tokens whenever it passed into declared routes

---
+++ backends/middlewares/jwt_auth_middleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const verifikasi_access_token = (req, res, next) => {
+    const { authorization } = req.headers;
+    const token = authorization && authorization.split(' ')[1];
+
+    if (!token) return res.status(401).json({
+        response: false,
+        message: 'Maaf, anda harus login terlebih dahulu!'
+    });
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(403).json({
+            response: false,
+            message: err.name === 'TokenExpiredError' ? 'Sesi anda sudah habis, silahkan generate akses token baru dengan refresh token anda kembali!' : 'Hayo deck, mau ngapain kmuh aowkaowk! heker yh banh? 😜😜'
+        });
+
+        console.log(user);
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = {
+    verifikasi_access_token
+};
