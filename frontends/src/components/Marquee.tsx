125a482977bb1bfc609a541ef0eca68c28675c00
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-15 11:24:05 +0700

    feat: adding marquee components for an interactive information

---
+++ frontends/src/components/Marquee.tsx
@@ -0,0 +1,15 @@
+interface MarqueeProps {
+    list_announcement: String
+}
+
+const Marquee = ({list_announcement}: MarqueeProps) => {
+	return (
+		<div className="flex items-center bg-[#FAAE2B] font-ibm-plex-mono-medium overflow-hidden border-b-2 border-t-2 border-black h-8">
+			<p className="animate-marquee">
+				{list_announcement}
+			</p>
+		</div>
+	);
+};
+
+export default Marquee;
