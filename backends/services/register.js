632c9370dafe77ccec0748acd4a38db318cbee88
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-16 17:31:40 +0700

    feat: adding error handling when interact with database, and also adding new register features for admin prodi + koordinator ta

--- backends/services/register.js
+++ backends/services/register.js
@@ -8,43 +8,42 @@ const kirim_link_verifikasi = async (req, res) => {
 	// mengambil data email dari user
 	const { email } = req.body;
 
-	// mengecek apakah email tersebut belum terdaftar tetapi sudah pernah dikirimin token
-	const resultInUnverifiedEmails = await db.query(
-		"SELECT * FROM unverified_email_register WHERE email = $1",
-		[email]
-	);
-	if (resultInUnverifiedEmails.rows.length > 0) {
-		// mengambil waktu expire token di database
-		const getExpiresAt = resultInUnverifiedEmails.rows[0].expires_at;
+	try {
+		// mengecek apakah email tersebut belum terdaftar tetapi sudah pernah dikirimin token
+		const resultInUnverifiedEmails = await db.query(
+			"SELECT * FROM unverified_email_register WHERE email = $1",
+			[email]
+		);
+		if (resultInUnverifiedEmails.rows.length > 0) {
+			// mengambil waktu expire token di database
+			const getExpiresAt = resultInUnverifiedEmails.rows[0].expires_at;
 
-		// mendapatkan waktu sekarang
-		const now = new Date();
+			// mendapatkan waktu sekarang
+			const now = new Date();
 
-		// Menghitung selisih antara kedua waktu
-		const diffTime = Math.abs(now - getExpiresAt);
+			// Menghitung selisih antara kedua waktu
+			const diffTime = Math.abs(now - getExpiresAt);
 
-		// konversi ke menit detik
-		const diffMinutes = Math.ceil(diffTime / (1000 * 60));
-		const diffSeconds = Math.ceil(diffTime / 1000);
+			// konversi ke menit detik
+			const diffMinutes = Math.ceil(diffTime / (1000 * 60));
+			const diffSeconds = Math.ceil(diffTime / 1000);
 
-		return res
-			.status(400)
-			.json({
+			return res.status(400).json({
 				response: false,
 				message: `Maaf, kami telah mengirimkan link verifikasi ke email tersebut sebelumnya coy, silahkan coba kembali sekitar ${diffMinutes}-menitan atau lebih tepatnya ${diffSeconds}-detik lagi ya coy!`,
 			});
-	}
+		}
 
-	// pembuatan token dan verifikasi link sekaligus isi email yang akan dikirim ke email user
-	const token = crypto.randomBytes(32).toString("hex");
-	const verificationLink = process.env.VERIFICATION_LINK + token;
+		// pembuatan token dan verifikasi link sekaligus isi email yang akan dikirim ke email user
+		const token = crypto.randomBytes(32).toString("hex");
+		const verificationLink = process.env.VERIFICATION_LINK + token;
 
-	const mailOptions = {
-		from: process.env.EMAIL_USER,
-		to: email,
-		subject:
-			"[SIPTATIF VERIFICATION] - Verifikasi Email untuk Registrasi Akun SIPTATIF",
-		html: `
+		const mailOptions = {
+			from: process.env.EMAIL_USER,
+			to: email,
+			subject:
+				"[SIPTATIF VERIFICATION] - Verifikasi Email untuk Registrasi Akun SIPTATIF",
+			html: `
         <html>
             <head>
                 <style>
@@ -120,56 +119,72 @@ const kirim_link_verifikasi = async (req, res) => {
             </body>
         </html>
         `,
-	};
+		};
 
-	transporter.sendMail(mailOptions, (error, info) => {
-		if (error) {
-			return res.status(500).json({
-				response: false,
-				message: "Maaf, sepertinya email anda tidak tepat!",
-			});
-		}
-	});
+		transporter.sendMail(mailOptions, (error, info) => {
+			if (error) {
+				return res.status(500).json({
+					response: false,
+					message: "Maaf, sepertinya email anda tidak tepat!",
+				});
+			}
+		});
 
-	// generate token expires
-	const expiresAt = new Date(Date.now() + 600000); // 10 minutes from now will expire
+		// generate token expires
+		const expiresAt = new Date(Date.now() + 600000); // 10 minutes from now will expire
 
-	// insert into unverified_emails with token
-	db.query(
-		"INSERT INTO unverified_email_register (email, verification_token, expires_at) VALUES ($1, $2, $3)",
-		[email, token, expiresAt]
-	);
+		// insert into unverified_emails with token
+		db.query(
+			"INSERT INTO unverified_email_register (email, verification_token, expires_at) VALUES ($1, $2, $3)",
+			[email, token, expiresAt]
+		);
 
-	return res.status(200).json({
-		response: true,
-		message: `Sukses mengirim link verifikasi ke email ${email}! silahkan cek, lalu tekan tombol 'Verifikasi Email Sekarang' untuk memverifikasinya!`,
-	});
+		return res.status(200).json({
+			response: true,
+			message: `Sukses mengirim link verifikasi ke email ${email}! silahkan cek, lalu tekan tombol 'Verifikasi Email Sekarang' untuk memverifikasinya!`,
+		});
+	} catch (err) {
+		return res.status(500).json({
+			response: false,
+			message: "Waduh, sepertinya ada kesalahan pada server kami!",
+		});
+	}
 };
 
 const verifikasi_token_register = async (req, res) => {
 	const { __token_verification } = req.body;
-	const results = await db.query(
-		"SELECT * FROM unverified_email_register WHERE verification_token = $1",
-		[__token_verification]
-	);
 
-	if (results.rows.length === 0) {
-		return res.status(400).json({
-			response: false,
+	try {
+		const results = await db.query(
+			"SELECT * FROM unverified_email_register WHERE verification_token = $1",
+			[__token_verification]
+		);
+
+		if (results.rows.length === 0) {
+			return res.status(400).json({
+				response: false,
+				message:
+					"Maaf, token anda tidak valid, sepertinya token anda telah expired, silahkan tekan tombol kirim link verifikasi kembali!",
+			});
+		}
+		// updating verified email status
+		await db.query(
+			"UPDATE unverified_email_register SET is_verified = true WHERE verification_token = $1",
+			[__token_verification]
+		);
+
+		res.json({
+			response: true,
 			message:
-				"Maaf, token anda tidak valid, sepertinya token anda telah expired, silahkan tekan tombol kirim link verifikasi kembali!",
+				"Selamat, token anda telah berhasil di verifikasi, kini anda diarahkan ke halaman pengisian form!",
+			results: results.rows[0],
+		});
+	} catch (err) {
+		return res.status(500).json({
+			response: false,
+			message: "Waduh, sepertinya ada kesalahan pada server kami!",
 		});
 	}
-
-	// updating verified email status
-    await db.query('UPDATE unverified_email_register SET is_verified = true WHERE verification_token = $1', [__token_verification]);
-
-	res.json({
-		response: true,
-		message:
-			"Selamat, token anda telah berhasil di verifikasi, kini anda diarahkan ke halaman pengisian form!",
-		results: results.rows[0],
-	});
 };
 
 // importing hash algorithm
@@ -179,35 +194,93 @@ const register_akun_mahasiswa = async (req, res) => {
 	// mengambil data email dari user
 	const { email, password, nim, nama, tanggal_lahir, no_hp } = req.body;
 
+	// hashing password with argon2 algorithm and catch error
+	let hashPassword;
+	try {
+		hashPassword = await argon2.hash(password);
+	} catch (err) {
+		return res.status(500).json({
+			response: false,
+			message: err.message,
+		});
+	}
+
+	// call procedure register_akun_mahasiswa to regist user account
+	try {
+		// query execution
+		await db.query("CALL register_akun_mahasiswa( $1, $2, $3, $4, $5, $6 )", [
+			email,
+			hashPassword,
+			nim,
+			nama,
+			tanggal_lahir,
+			no_hp,
+		]);
+		// return success response
+		return res.status(200).json({
+			response: true,
+			message: `Yeay, registrasi sukses!, sebentar ya, kami akan mengarahkan kamu kehalaman login.`,
+		});
+	} catch (err) {
+		const errCode = err?.code;
+		if (errCode === "23505") {
+			// unique_violation
+			return res.status(500).json({
+				response: false,
+				message: `Registrasi Gagal! ${err.detail
+					.replace("Kunci", "Data anda, yakni")
+					.replace("sudah ada.", "sudah pernah didaftarkan.")}`,
+			});
+		} else {
+			return res.status(500).json({
+				response: false,
+				message: `Registrasi Gagal! ${err.message}`,
+			});
+		}
+	}
+};
+
+const register_akun_admin_prodi = async (req, res) => {
+    // mengambil data email dari user
+    const { email, password, secret_key } = req.body;
+    if (secret_key !== process.env.ADMIN_PRODI_SECRET_KEY) {
+        return res.status(403).json({
+            response: false,
+            message: "Hayo deck, mau ngapain kmuh aowkaowk! heker yh banh? 😜😜",
+        });
+    }
     // hashing password with argon2 algorithm and catch error
     let hashPassword;
-	try {
+    try {
         hashPassword = await argon2.hash(password);
-	} catch (err) {
+    } catch (err) {
         return res.status(500).json({
             response: false,
             message: err.message,
-        });	
+        });
     }
-
     // call procedure register_akun_mahasiswa to regist user account
-    try{
+    try {
         // query execution
-        await db.query(
-            "CALL register_akun_mahasiswa( $1, $2, $3, $4, $5, $6 )",
-            [email, hashPassword, nim, nama, tanggal_lahir, no_hp]
-        );
+        await db.query("INSERT INTO AKUN(email, password, id_role) VALUES( $1, $2, $3 )", [
+            email,
+            hashPassword,
+            1,
+        ]);
         // return success response
         return res.status(200).json({
             response: true,
-            message: `Yeay, registrasi sukses!, sebentar ya, kami akan mengarahkan kamu kehalaman login.`,
+            message: `Yeay, registrasi sukses!, kini anda dapat menggunakan akun ${email} dengan role admin prodi untuk login.`,
         });
     } catch (err) {
         const errCode = err?.code;
-        if (errCode === "23505") { // unique_violation
+        if (errCode === "23505") {
+            // unique_violation
             return res.status(500).json({
                 response: false,
-                message: `Registrasi Gagal! ${err.detail.replace("Kunci", "Data anda, yakni").replace("sudah ada.", "sudah pernah didaftarkan.")}`,
+                message: `Registrasi Gagal! ${err.detail
+                    .replace("Kunci", "Data anda, yakni")
+                    .replace("sudah ada.", "sudah pernah didaftarkan.")}`,
             });
         } else {
             return res.status(500).json({
@@ -216,10 +289,58 @@ const register_akun_mahasiswa = async (req, res) => {
             });
         }
     }
-};
+}
+
+const register_akun_koordinator_ta = async (req, res) => {
+    // mengambil data email dari user
+    const { email, password } = req.body;
+
+    // hashing password with argon2 algorithm and catch error
+    let hashPassword;
+    try {
+        hashPassword = await argon2.hash(password);
+    } catch (err) {
+        return res.status(500).json({
+            response: false,
+            message: err.message,
+        });
+    }
+    // call procedure register_akun_mahasiswa to regist user account
+    try {
+        // query execution
+        await db.query("INSERT INTO AKUN(email, password, id_role) VALUES( $1, $2, $3 )", [
+            email,
+            hashPassword,
+            2,
+        ]);
+        // return success response
+        return res.status(200).json({
+            response: true,
+            message: `Yeay, registrasi sukses!, kini anda dapat menggunakan akun ${email} dengan role koordinator ta untuk login.`,
+        });
+    } catch (err) {
+        const errCode = err?.code;
+        if (errCode === "23505") {
+            // unique_violation
+            return res.status(500).json({
+                response: false,
+                message: `Registrasi Gagal! ${err.detail
+                    .replace("Kunci", "Data anda, yakni")
+                    .replace("sudah ada.", "sudah pernah didaftarkan.")}`,
+            });
+        } else {
+            return res.status(500).json({
+                response: false,
+                message: `Registrasi Gagal! ${err.message}`,
+            });
+        }
+    }
+}
 
 module.exports = {
 	verifikasi_token_register,
 	kirim_link_verifikasi,
 	register_akun_mahasiswa,
+    register_akun_admin_prodi,
+	register_akun_koordinator_ta
 };
