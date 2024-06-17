62516e1d64446a064425f53558ffe1975085d894
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-17 11:35:01 +0700

    feat: adding 3 types of get dosen, there are normal hit (returning all data), search based hit, and pagination hit, then retrieve nidn from params not from body in update dosen, and also auto deleting koor ta akun (if available) when deleting dosen data

--- backends/controllers/dosen_controller.js
+++ backends/controllers/dosen_controller.js
@@ -22,22 +22,37 @@ const getDosen = async (req, res) => {
 		// get all dosen from database with pagination logic, if search query is not empty, then add where search logic
 		let results;
 		let countData;
+
+		// bakal query execute mode searching
 		if (search) {
 			regexSearch = `%${search.toLowerCase()}%`;
 			results = await db.query(
-				"SELECT * FROM dosen WHERE LOWER(nama) LIKE $1 OR LOWER(nidn) LIKE $1 ORDER BY nama LIMIT $2 OFFSET $3",
+				"SELECT * FROM dosen WHERE LOWER(nama) LIKE $1 OR LOWER(nidn) LIKE $1 ORDER BY CASE WHEN nama LIKE 'Prof. Dr.%' THEN 1 WHEN nama LIKE 'Dr.%' THEN 2 ELSE 3 END, nama LIMIT $2 OFFSET $3",
 				[regexSearch, limit, offset]
 			);
 			countData = await db.query(
 				"SELECT COUNT(*) FROM dosen WHERE LOWER(nama) LIKE $1 OR LOWER(nidn) LIKE $1",
 				[regexSearch]
 			);
-		} else {
-			results = await db.query("SELECT * FROM dosen ORDER BY nama LIMIT $1 OFFSET $2", [
+		} 
+		
+		// bakal query execute mode paging
+		else if (page) {
+			results = await db.query("SELECT * FROM dosen ORDER BY CASE WHEN nama LIKE 'Prof. Dr.%' THEN 1 WHEN nama LIKE 'Dr.%' THEN 2 ELSE 3 END, nama LIMIT $1 OFFSET $2", [
 				limit,
 				offset,
 			]);
 			countData = await db.query("SELECT COUNT(*) FROM dosen");
+		} 
+		
+		// bakal query execute mode normal, langsung terobos semua data jika tak ada query search/page
+		else {
+			results = await db.query("SELECT * FROM dosen ORDER BY CASE WHEN nama LIKE 'Prof. Dr.%' THEN 1 WHEN nama LIKE 'Dr.%' THEN 2 ELSE 3 END, nama");
+			return res.status(200).json({
+				response: true,
+				message: "Berikut data dosen yang telah didaftarkan sebelumnya!",
+				results: results.rows,
+			});
 		}
 
 		// get total data of dosen and calculate total possible pages
@@ -106,7 +121,8 @@ const createDosen = async (req, res) => {
 };
 
 const updateDosen = async (req, res) => {
-	const { nidn, nama, no_hp, jenis_kelamin, email } = req.body;
+	const { nidn } = req.params;
+	const { nama, no_hp, jenis_kelamin, email } = req.body;
 
 	try {
 		const results = await db.query(
@@ -136,7 +152,13 @@ const deleteDosen = async (req, res) => {
 	const { nidn } = req.params;
 
 	try {
+		// delete juga akun koordinator ta di table akun jika ada
+		const getEmail = await db.query("SELECT email FROM dosen WHERE nidn = $1", [nidn]);
+		if (getEmail.rows[0]) await db.query("DELETE FROM akun WHERE email = $1", [getEmail.rows[0].email]);
+		
+		// baru delete data dosen di tabel dosennya
 		const results = await db.query("DELETE FROM dosen WHERE nidn = $1", [nidn]);
+
 		if (results.rowCount === 0) {
 			return res.status(400).json({
 				response: false,
