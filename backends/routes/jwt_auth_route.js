6c4f012d59204492034ac06eb05b3ee4f6f3c5e2
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-18 17:07:47 +0700

    feat: adding route for verifying access token whenever user entered frontend routes to verify if them roles is allowed, so backend will returns role in true response

--- backends/routes/jwt_auth_route.js
+++ backends/routes/jwt_auth_route.js
@@ -6,11 +6,24 @@ const router = app.Router(); // importing express router
 const { 
     refresh_access_token
 } = require("../services/jwt_auth");
+const { verifikasi_access_token } = require("../middlewares/jwt_auth_middleware");
 
 // list available routes in login features
 router.post(
     "/refresh-access-token", 
     refresh_access_token
+);
+
+router.post(
+    "/verify-access-token",
+    verifikasi_access_token,
+    (req, res) => {
+        res.status(200).json({
+            response: true,
+            message: "access token is verified",
+            role: req.user.role
+        })
+    }
 );
 
 // export all defined router
