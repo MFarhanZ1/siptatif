03f6c7f509a88df061c76c830a7047b22a8e540d
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-19 10:18:32 +0700

    perf: adding left join method in order to still retrieve null value at keahlian dosen instead of unvisibiltying them

--- databases/views/dosen_views.sql
+++ databases/views/dosen_views.sql
@@ -23,19 +23,18 @@ ORDER BY
 -- data detail dosen view untuk format "Cyber Security, Machine Learning (ML), Mobile" (nanti digunakan untuk detail penguji dan pembimbing)
 CREATE OR REPLACE VIEW view_detail_dosen AS
 SELECT 
-    dosen.nidn "nidn", 
-    dosen.nama "nama",
-    dosen.no_hp "no_hp",
-    dosen.jenis_kelamin "jenis_kelamin",
-    dosen.email "email",
+    dosen.nidn AS "nidn", 
+    dosen.nama AS "nama",
+    dosen.no_hp AS "no_hp",
+    dosen.jenis_kelamin AS "jenis_kelamin",
+    dosen.email AS "email",
     STRING_AGG(keahlian.nama, ', ') AS keahlian
 FROM 
-    keahlian_dosen kd,
-    dosen,
-    keahlian
-WHERE
-    kd.id_keahlian = keahlian.id AND
-    kd.nidn = dosen.nidn
+    dosen
+LEFT JOIN 
+    keahlian_dosen kd ON dosen.nidn = kd.nidn
+LEFT JOIN 
+    keahlian ON kd.id_keahlian = keahlian.id
 GROUP BY 
     dosen.nidn, 
     dosen.nama
