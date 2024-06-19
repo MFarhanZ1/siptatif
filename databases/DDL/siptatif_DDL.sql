c931cdabbeed247c573b2e9c159506a303fea85c
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-20 01:46:30 +0700

    fix: repair forgotted ddl atribut changed when migrating procedure type data

--- databases/DDL/siptatif_DDL.sql
+++ databases/DDL/siptatif_DDL.sql
@@ -108,7 +108,7 @@ CREATE TABLE tugas_akhir (
 	jenis_pendaftaran		TYPE_JENIS_PENDAFTARAN NOT NULL,
 	judul_ta				VARCHAR(255) NOT NULL,
 	kategori_ta				TYPE_KATEGORI_TA NOT NULL,
-	berkas					BYTEA NOT NULL,
+	berkas					VARCHAR(255) NOT NULL,
 	catatan					TEXT,
 	status 					TYPE_STATUS DEFAULT 'MENUNGGU',
 	timestamp 				TIMESTAMP DEFAULT NOW(),					
@@ -130,22 +130,20 @@ CREATE TYPE TYPE_STATUS_RIWAYAT AS ENUM ('SELESAI', 'BELUM');
 
 DROP TABLE IF EXISTS riwayat_pembimbing CASCADE;
 CREATE TABLE riwayat_pembimbing (
-	id			SERIAL NOT NULL,
-	no_reg_ta	VARCHAR(9) NOT NULL,
+	no_reg_ta	VARCHAR(10) NOT NULL,
 	nidn		VARCHAR(20) NOT NULL,
 	status		TYPE_STATUS_RIWAYAT DEFAULT 'BELUM',
-	CONSTRAINT 	PK_Riwayat_Pembimbing PRIMARY KEY (id),
+	CONSTRAINT 	PK_Riwayat_Pembimbing PRIMARY KEY (no_reg_ta, nidn),
 	CONSTRAINT 	FK_Ripem_TA FOREIGN KEY (no_reg_ta) REFERENCES tugas_akhir(no_reg_ta),
 	CONSTRAINT 	FK_Ripem_Dosen FOREIGN KEY(nidn) REFERENCES dosen (nidn)
 );
 --pembuatan TABLE riwayat_penguji
 DROP TABLE IF EXISTS riwayat_penguji CASCADE;
 CREATE TABLE riwayat_penguji (
-	id			SERIAL NOT NULL,
-	no_reg_ta	VARCHAR(9) NOT NULL,
+	no_reg_ta	VARCHAR(10) NOT NULL,
 	nidn		VARCHAR(20) NOT NULL,
 	status		TYPE_STATUS_RIWAYAT DEFAULT 'BELUM',
-	CONSTRAINT 	PK_Riwayat_Penguji PRIMARY KEY (id),
+	CONSTRAINT 	PK_Riwayat_Penguji PRIMARY KEY (no_reg_ta, nidn),
 	CONSTRAINT 	FK_Riuji_TA FOREIGN KEY (no_reg_ta) REFERENCES tugas_akhir (no_reg_ta),
 	CONSTRAINT 	FK_Riuji_Dosen FOREIGN KEY (nidn) REFERENCES dosen (nidn)
 );
