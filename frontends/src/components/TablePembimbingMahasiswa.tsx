601d0fc49986ac969ca1d7e935df99a3af858474
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-21 01:35:33 +0700

    feat: adding more readable and informative pembimbing list table in mahasiswa roles

--- frontends/src/components/TablePembimbingMahasiswa.tsx
+++ frontends/src/components/TablePembimbingMahasiswa.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 import Pagination from "./Pagination";
 import SeachField from "./SeachField";
-import { getDataDosenMahasiswaPage } from "../services/MahasiswaService";
+import { getDataDosenMahasiswaPage, getDataDosenMahasiswaSearch } from "../services/MahasiswaService";
 
 function TablePembimbingMahasiswa() {
   const [body, setBody] = useState([]);
@@ -12,6 +12,8 @@ function TablePembimbingMahasiswa() {
   const [totalItems, setTotalItems] = useState(0);
   const [totalPage, setTotalPage] = useState(0);
 
+  const [searchData, setSearchData] = useState("");
+
   useEffect(() => {
     getDataDosenMahasiswaPage(1).then((data) => {
       setBody(data.results);
@@ -20,13 +22,29 @@ function TablePembimbingMahasiswa() {
       setTotalPage(data.info.total_page);
     });
   }, [page]);
+
+  useEffect(() => {
+    getDataDosenMahasiswaSearch(searchData).then((data) => {
+      setBody(data.results);
+      setTotalItems(data.info.total_all_data);
+      setPageInterval(data.info.data_per_page);
+      setTotalPage(data.info.total_page);
+    });
+  }, [searchData]);
+
   const redItem: number = 1;
+  
   return (
-    <div className="flex flex-col gap-4 overflow-x-auto font-poppins flex-1 m-3">
+    <div className="flex flex-col px-4 gap-3 overflow-x-auto font-poppins h-full">
       <div>{/* <SeachField /> */}</div>
-      <div className="overflow-y-auto border border-black">
+        <SeachField 
+          onChange={(e) => {
+            setSearchData(e as SetStateAction<string>);
+          }}
+        />
+      <div className="overflow-y-auto h-full border border-black">
         {/* table */}
-        <table className="w-full text-xs text-left table-auto">
+        <table className="w-full text-xs text-left table-auto flex-grow">
           <thead className="bg-gray-700 uppercase text-white sticky top-0 w-full">
             <tr className="text-center text-sm">
               <th>NIDN</th>
@@ -58,6 +76,23 @@ function TablePembimbingMahasiswa() {
                     return "bg-red-400";
                   } else if (persentase >= 0.7 && persentase <= 0.9) {
                     return "bg-red-600";
+                  } else {
+                    return "bg-green-400";
+                  }
+                };
+                const detailStatusClass = () => {
+                  if (persentase == redItem) {
+                    return "😭 Yah, Penuh!";
+                  } else if (persentase >= 0.1 && persentase <= 0.3) {
+                    return "🤠 Masih, Tersedia Nih!";
+                  } else if (persentase >= 0.3 && persentase <= 0.6) {
+                    return "😫 Hampir Penuh!";
+                  } else if (persentase >= 0.6 && persentase <= 0.7) {
+                    return "😒 Ayo, Keburu Penuh!";
+                  } else if (persentase >= 0.7 && persentase <= 0.9) {
+                    return "😜 Gercep, Mau Penuh!";
+                  } else {
+                    return "😀 Tersedia Banyak Nih!";
                   }
                 };
                 return (
@@ -93,8 +128,11 @@ function TablePembimbingMahasiswa() {
                         </div>
                       }
                     </td>
-                    <td className="px-6 py-2 truncate max-w-36">
-                      {"tersedia"}
+                    <td className="px-6 py-2 truncate max-w-24">
+                      <div className={`${bgColorClass()} rounded-xl font-poppins font-semibold bg-opacity-50 border border-black p-4 flex justify-center items-center h-4`}>
+
+                      {detailStatusClass()}
+                      </div>
                     </td>
                   </tr>
                 );
