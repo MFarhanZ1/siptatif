105ad409fd1b3f31c28e8e378eda6273711750a9
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-20 10:32:44 +0700

    feat: adding placeholders params at search field

--- frontends/src/components/SeachField.tsx
+++ frontends/src/components/SeachField.tsx
@@ -2,14 +2,16 @@ import iconsearch from "../../assets/icons/searchfield.svg"
 
 interface SeachFieldProps {
   // eslint-disable-next-line @typescript-eslint/ban-types
-  onChange: (search: String) => void
+  onChange: (search: String) => void;
+  placeholder?: string;
 }
 
-function SeachField({onChange}: SeachFieldProps) {
+function SeachField({onChange, placeholder}: SeachFieldProps) {
+  placeholder = placeholder ? placeholder : 'Cari berdasarkan NIDN, Nama Dosen, ataupun Keahlian...'
   return (
     <div className='flex gap-2 font-poppins rounded-md border border-black p-1'>
         <img src={iconsearch} alt="" className='w-8'/>
-        <input onChange={(e) => onChange(e.target.value)} className='w-full bg-transparent border-none focus:outline-none' type="text" name="" id=""  placeholder='Cari berdasarkan NIDN, Nama Dosen, Keahlian'/>
+        <input onChange={(e) => onChange(e.target.value)} className='w-full bg-transparent border-none focus:outline-none' type="text" name="" id=""  placeholder={placeholder}/>
     </div>
   )
 }
