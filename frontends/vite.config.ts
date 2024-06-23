e4cfd6e0fc50e563622cb0a24cf00ac404a6a845
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-23 18:13:15 +0700

    feat: adding server options at vite config, so it can runs in all ip

--- frontends/vite.config.ts
+++ frontends/vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    server: {
+      host: '0.0.0.0',
+      port: 5173
+    },  
     define: {
       'process.env.BASE_URL': JSON.stringify(env.BASE_URL)
     },
