1d8c86b52c6f03791760011fc805226ae9f35002
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-23 18:15:51 +0700

    perf: adding responsive footer components in mobile version

--- frontends/src/components/Footer.tsx
+++ frontends/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="bg-[#E1E1E1] text-black h-8 flex items-center justify-center border-t border-black">
-            <p className="font-ibm-plex-mono-medium">&copy; {new Date().getFullYear()} - made with 💙 from SIPTATIF DEV.</p>
+            <p className="text-xs sm:text-base font-ibm-plex-mono-medium">&copy; {new Date().getFullYear()} - made with 💙 from SIPTATIF DEV.</p>
         </footer>
     );
 }
