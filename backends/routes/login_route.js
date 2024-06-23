e04583e586c5e54421f6d2a1e7e1a2956f883c31
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-06-23 18:36:34 +0700

    feat: adding login endpoint and controller for mobile apps

--- backends/routes/login_route.js
+++ backends/routes/login_route.js
@@ -4,13 +4,18 @@ const router = app.Router(); // importing express router
 
 // importing services
 const { 
-    verifikasi_login 
+    verifikasi_login, 
+    verifikasi_login_mobile
 } = require("../services/login");
 
 // list available routes in login features
 router.post(
     "/login", 
     verifikasi_login
+);
+router.post(
+    "/mobile/login", 
+    verifikasi_login_mobile
 );
 
 // export all defined router
